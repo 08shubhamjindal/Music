82485c1535223f9f751832a5412acd4628376dab
SH20088207 <sh20088207@wipro.com>
2020-02-25 12:02:04 +0530

    added search functionlity

--- Music.js
+++ Music.js
@@ -1,32 +1,46 @@
 window.onload = async function() {
-  var getjsondata = await getUserAsync();
-  console.log(getjsondata.toptracks.track[1].image[1]['#text']);
-  var list = [];
-  for (user of getjsondata.toptracks.track){
-      list.push(
-         `<div class="card">
-         <img src=${user.image[0]['#text']} alt="Avatar" style="width:100%">
-          <div class="container">
-          <h4><b>${user.name}</b></h4>
-          <p>${user.playcount}</p>
-          <a href=${user.url} target="_blank">Visit Audio Link</a>
-          </div></div>`);
-    }
-     document.getElementById("list").innerHTML += list + "<br/>";
+     getsearchKeyWord();
 }
-async function getUserAsync(){
+async function getsearchKeyWord(){
+  var searchKeyWord = document.getElementById("setsearchkeyword").value;
+  var getjsondata;
+  if(searchKeyWord==""){
+       getjsondata =  await getUserAsync("");
+  }else{
+       getjsondata =  await getUserAsync(searchKeyWord);
+  }
+  console.log(getjsondata.toptracks.track[1].image[1]['#text']);
+   var list = [];
+  document.getElementById("list").innerHTML = "";
+  for (user of getjsondata.toptracks.track){
+      list.push(`<ul class="cards">
+    <li class="cards__item">
+    <div class="card">
+      <div class="card__image card__image--fence"></div>
+      <div class="card__content">
+        <div class="card__title">${user.name}</div>
+        <img src=${user.image[1]['#text']} alt="Avatar" style="width:100%">
+        <a href=${user.url} target="_blank">Visit Audio Link</a>
+        <button class="btn btn--block card__btn">Button</button>
+      </div>
+    </div>
+  </li>
+</ul>`);
+    }
+    document.getElementById("list").innerHTML += list + "<br/>";
+}
+
+async function getUserAsync(searchKeyWord){
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=cher&api_key=2f88bfdd53048e6731918bd0a1480023&format=json";
+    var url;
+    if(searchKeyWord==""){
+       url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=cher&api_key=2f88bfdd53048e6731918bd0a1480023&format=json";
+    }else{
+       url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+searchKeyWord+"&api_key=2f88bfdd53048e6731918bd0a1480023&format=json";
+       console.log(url);
+    }
     const response= await fetch(proxyurl + url)
     const data = await response.json()
     console.log(data);
     return data;
 }
-// <li>
-// <h1>${user.name}</h1>
-// <p>${user.playcount}</p>
-// <p>${user.listeners}</p>
-// <p>${user.listeners}</p>
-// <p>${user.mbid}</p>
-// <a href=${user.url} target="_blank">Visit Movie Link</a>
-  // </li>
